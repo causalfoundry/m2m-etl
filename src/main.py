b2730807740bdfd763877fe2a978847312f02c88
dexian <dxe.tang@gmail.com>
2024-11-29 18:09:55 +0100

    add env

--- src/main.py
+++ src/main.py
@@ -2,6 +2,4 @@ import os
 
 if __name__ == '__main__':
     print('ETL starting')
-    for key, value in os.environ.items():
-        if key.startswith("ETL_"):
-            print(f"{key}: {value}")
+    print(os.environ.get("input"))
