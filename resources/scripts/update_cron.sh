f475d3e520c9e5dae8614349a6617ca11afca90c
dexian <dxe.tang@gmail.com>
2024-11-29 17:25:55 +0100

    debug

--- resources/scripts/update_cron.sh
+++ resources/scripts/update_cron.sh
@@ -18,10 +18,10 @@ yq eval '. | keys' $file_path | sed 's/- //' | while read -r NAME; do
     yq "
       .metadata.name=\"$NAME\" |
       .metadata.namespace=\"$PROJECT_ID\" |
-      .spec.template.spec.containers[0].image=\"$IMAGE\" |
-      .spec.template.spec.containers[0].resources.limits.cpu=$CPU |
-      .spec.template.spec.containers[0].resources.limits.memory=\"$MEMORY\" |
-      .spec.template.spec.serviceAccountName=\"$SERVICE_ACCOUNT\"
+      .spec.template.spec.template.spec.containers[0].image=\"$IMAGE\" |
+      .spec.template.spec.template.spec.containers[0].resources.limits.cpu=\"$CPU\" |
+      .spec.template.spec.template.spec.containers[0].resources.limits.memory=\"$MEMORY\" |
+      .spec.template.spec.template.spec.serviceAccountName=\"$SERVICE_ACCOUNT\"
     " ./resources/job_template.yml > ./resources/$NAME.yml
     echo '-------------'
     cat ./resources/$NAME.yml
